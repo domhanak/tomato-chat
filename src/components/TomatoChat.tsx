d260e968164281375eb8810f5ee65b7facf3eed3
domhanak <domin.hanak@gmail.com>
2018-10-16 10:39:46 +0200

    Initial commit with configuration files
    Add webpack configuration file
    Add jest configuration file
    Add typescript configuration files
    Add babel and eslintr configuration files
    Rename base component to TomatoChat

---
+++ src/components/TomatoChat.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import '../styles/index.css';
+import { LoginForm } from './LoginForm';
+import * as React from 'react';
+
+export class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  render(): JSX.Element {
+    return (
+      <div className="app">
+          <h1 className="page-header"> Tomato Chat Application </h1>
+          <LoginForm />
+      </div>
+    );
+  }
+}
