df3be4ff6a36f7a2e256c4ce8806f5400f797ae2
domhanak <domin.hanak@gmail.com>
2018-10-16 10:42:39 +0200

    Add LoginForm, ChatWindow, MessageForm components
    Add basic skeleton for ChatWindow and MessageForm
    add LoginForm with dummy implementation and basic css

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import '../styles/index.css';
+
+export class LoginForm extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    // Add methods handling inputs
+
+    render(): JSX.Element {
+        return (
+                <form className="login-form">
+                    <label className="message-form__login-label"
+                           htmlFor="login" > Username: </label>
+                    <input type="text"
+                           id="login"
+                           className="login-form__login" />
+                    <label className="message-form__password-label"
+                           htmlFor="login"> Password: </label>
+                    <input type="text"
+                           id="login"
+                           className="login-form__login"/>
+                    <button type="submit"> Login </button>
+                </form>
+        );
+    }
+}
