50b79f5bded3244cfb82c1581a1b812f5cde49d6
Sevitas <sevcik.martin4@gmail.com>
2018-10-17 13:27:51 +0200

    Sass was introduced, webpack setting up for sass, css files replaced with scss alternatives, new structure for scss files was introduced

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ require.context('../public/', true);
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './styles/index.css';
+import './styles/base.scss';
 import 'babel-core/register';
 import 'babel-polyfill';
 import { TomatoChat } from './components/TomatoChat.tsx';
