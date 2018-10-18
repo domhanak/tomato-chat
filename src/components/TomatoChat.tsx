937ba1d6731d6068eb0b0fcc68038274b5e38c04
Sevitas <sevcik.martin4@gmail.com>
2018-10-18 14:46:18 +0200

    Routing was introduced

--- src/components/TomatoChat.tsx
+++ src/components/TomatoChat.tsx
@@ -2,6 +2,9 @@ import { Component } from 'react';
 import '../styles/base.scss';
 import { LoginForm } from './LoginForm';
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Profile} from './Profile';
+import {ChatWindow} from './ChatWindow';
 
 export class TomatoChat extends Component {
   constructor(props: any) {
@@ -15,6 +18,23 @@ export class TomatoChat extends Component {
     return (
       <div className="app">
           <h1 className="page-header"> Tomato Chat Application </h1>
+          <Router>
+            <div>
+              <ul>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                    <Link to="/messages">Messages</Link>
+                </li>
+              </ul>
+
+              <hr />
+
+              <Route path="/profile" component={Profile}/>
+              <Route path="/messages" component={ChatWindow}/>
+            </div>
+          </Router>
           <LoginForm />
       </div>
     );
