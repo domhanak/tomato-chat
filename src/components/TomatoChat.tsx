664998c1d37e5d85e817e71ee58931206a494f62
Sevitas <sevcik.martin4@gmail.com>
2018-10-18 15:24:50 +0200

    routing introduced, sass files refactored

--- src/components/TomatoChat.tsx
+++ src/components/TomatoChat.tsx
@@ -2,41 +2,59 @@ import { Component } from 'react';
 import '../styles/base.scss';
 import { LoginForm } from './LoginForm';
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {Profile} from './Profile';
 import {ChatWindow} from './ChatWindow';
 
 export class TomatoChat extends Component {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
-  render(): JSX.Element {
-    return (
-      <div className="app">
-          <h1 className="page-header"> Tomato Chat Application </h1>
-          <Router>
-            <div>
-              <ul>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                    <Link to="/messages">Messages</Link>
-                </li>
-              </ul>
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+    }
 
-              <hr />
+    render(): JSX.Element {
+        return (
+            <div className="app">
+                <header>
+                    <h1 className="page-header"> Tomato Chat Application </h1>
+                    <Router>
+                        <div>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/login">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/profile">Profile</Link>
+                                </li>
+                                <li>
+                                    <Link to="/messages">Messages</Link>
+                                </li>
+                                <li>
+                                    <Link to="/logout">Logout</Link>
+                                </li>
+                            </ul>
 
-              <Route path="/profile" component={Profile}/>
-              <Route path="/messages" component={ChatWindow}/>
+                            <hr />
+
+                            <Route path="/"/>
+                            <Route path="/login" component={LoginForm}/>
+                            <Route path="/profile" component={Profile}/>
+                            <Route path="/messages" component={ChatWindow}/>
+                        </div>
+                    </Router>
+                </header>
+                <div>
+                    <p>
+                        Chat application so you can chat like true tomato.
+                    </p>
+                </div>
             </div>
-          </Router>
-          <LoginForm />
-      </div>
-    );
-  }
+        );
+    }
 }
