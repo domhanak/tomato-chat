ced37bfd25f1acc5818b79d63e6b9b155ae215bd
Sevitas <sevcik.martin4@gmail.com>
2018-10-19 14:19:56 +0200

    Channel component with sass file was introduced.

---
+++ src/components/Channel.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class Channel extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render(): JSX.Element {
+        return (
+            <div>channel</div>
+        );
+    }
+}
