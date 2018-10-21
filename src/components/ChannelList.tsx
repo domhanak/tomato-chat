0b3f73293e86d8c499cd505118da67c4e08397af
Sevitas <sevcik.martin4@gmail.com>
2018-10-21 12:41:49 +0200

    channel list fix

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import {List} from 'immutable';
 import {ChangeEvent} from 'react';
-import {IChannel} from '../models/IChannel';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import {ChatWindow} from './ChatWindow';
 import {Channel} from './Channel';
 
 export class ChannelList extends React.Component {
@@ -11,7 +9,7 @@ export class ChannelList extends React.Component {
         super(props);
         this.state = {
             newChannelName: '',
-            channelList: List<IChannel>()
+            channelList: List<string>()
         };
     }
 
@@ -58,41 +56,12 @@ export class ChannelList extends React.Component {
                                         <a onClick={this.moveDown} className="arrowDown glyphicon glyphicon-arrow-down" />
                                     </div>
 
-                                    <Route path="/messages" component={ChatWindow}/>
-                                    <Route path="/channel" component={Channel}/>
-                                </div>
-                            </Router>
-                        </li>
-                        <li key="2">
-                            <Router>
-                                <div>
-                                    <h6><Link className="channel-name" to="/todo">Channel 1</Link></h6>
-                                    <div className="channel-options visible">
-                                        <Link to="/channel" className="settings glyphicon glyphicon-cog" />
-                                        <a onClick={this.moveUp} className="arrowUp glyphicon glyphicon-arrow-up" />
-                                        <a onClick={this.moveDown} className="arrowDown glyphicon glyphicon-arrow-down" />
-                                    </div>
 
-                                    <Route path="/messages" component={ChatWindow}/>
                                     <Route path="/channel" component={Channel}/>
                                 </div>
                             </Router>
                         </li>
-                        <li key="3">
-                            <Router>
-                                <div>
-                                    <h6><Link className="channel-name" to="/todo">Channel 3</Link></h6>
-                                    <div className="channel-options visible">
-                                        <Link to="/channel" className="settings glyphicon glyphicon-cog" />
-                                        <a onClick={this.moveUp} className="arrowUp glyphicon glyphicon-arrow-up" />
-                                        <a onClick={this.moveDown} className="arrowDown glyphicon glyphicon-arrow-down" />
-                                    </div>
 
-                                    <Route path="/messages" component={ChatWindow}/>
-                                    <Route path="/channel" component={Channel}/>
-                                </div>
-                            </Router>
-                        </li>
                     </ul>
                 </div>
                 <div className="channel-creation">
