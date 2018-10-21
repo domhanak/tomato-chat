4662ffa58f94add03f7c46d95e7bd5d06434cd9d
Sevitas <sevcik.martin4@gmail.com>
2018-10-21 22:45:08 +0200

    scss fixes

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -38,37 +38,17 @@ export class ChatWindow extends React.Component<IChatWindowProps, IChatWindowSta
     render(): JSX.Element {
         return (
             <div id="chat-container">
-                <div className="row">
-<<<<<<< HEAD
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-<<<<<<< HEAD
-                        <h2> Channel List </h2>
-=======
->>>>>>> f73814a0fe7f737db05d33b9e4be30d202158ac0
-||||||| merged common ancestors
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-=======
-                    <div className="col-lg-4 col-md-4 col-sm-4 channel-container">
->>>>>>> 1089d56aa8f9700a2d703b7e1cd96ffbffb5d5f8
+                <div className="col-lg-4 col-md-4 col-sm-4">
+                    <div className="channel-container">
                         <ChannelList/>
                     </div>
-                    <div className="col-lg-8 col-md-8 col-sm-8 message-container">
+                </div>
+                <div className="col-lg-8 col-md-8 col-sm-8 new-message-container">
+                    <div className="message-container">
                         <h4> Channel Name </h4>
                         <MessageList messages={this.state.messages}/>
                     </div>
-                </div>
-<<<<<<< HEAD
-                <div className="new-message row">
-<<<<<<< HEAD
-                    <div className="col-lg-8 col-md-8 col-sm-8">
-=======
-||||||| merged common ancestors
-                <div className="new-message row">
-=======
-                <div className="new-message-container">
->>>>>>> 1089d56aa8f9700a2d703b7e1cd96ffbffb5d5f8
-                    <div className="col-lg-8 col-md-8 col-sm-8 col-md-offset-4">
->>>>>>> f73814a0fe7f737db05d33b9e4be30d202158ac0
+                    <div>
                         <MessageForm message={this.state.message} username={this.state.username} onMessageChange={this.updateMessage} onSend={this.sendMessage}/>
                     </div>
                 </div>
