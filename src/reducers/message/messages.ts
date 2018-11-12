c19c6b5f62580b46f244f1510b9e87c1f3cee5e6
domhanak <domin.hanak@gmail.com>
2018-11-12 01:20:13 +0100

    Fix Message model name and refresh upon message add

--- src/reducers/message/messages.ts
+++ src/reducers/message/messages.ts
@@ -3,7 +3,7 @@ import { combineReducers} from 'redux';
 import { IMessages } from '../../models/ITomatoApp';
 import { IMessage } from '../../models/IMessage';
 import {
-    TOMATO_APP_LOADING_MESSAGES_STARTED, TOMATO_APP_LOADING_MESSAGES_SUCCESS,
+    TOMATO_APP_LOADING_MESSAGES_STARTED, TOMATO_APP_LOADING_MESSAGES_SUCCESS, TOMATO_APP_MESSAGE_CREATE_SUCCESS,
 } from '../../constants/actionTypes';
 
 const messagesById = (prevState = Immutable.Map<Uuid, IMessage>(), action: Action): Immutable.Map<Uuid, IMessage> => {
@@ -12,6 +12,8 @@ const messagesById = (prevState = Immutable.Map<Uuid, IMessage>(), action: Actio
             return prevState;
         case TOMATO_APP_LOADING_MESSAGES_SUCCESS:
             return Immutable.Map(action.payload.messages.map((message: IMessage) => [message.id, message]));
+        case TOMATO_APP_MESSAGE_CREATE_SUCCESS:
+            return prevState.set(action.payload.message.id, action.payload.message);
         default:
             return prevState;
     }
@@ -23,6 +25,8 @@ const allMessagesByIds = (prevState: Immutable.List<Uuid> = Immutable.List(), ac
             return prevState;
         case TOMATO_APP_LOADING_MESSAGES_SUCCESS:
             return Immutable.List(action.payload.messages.map((message: IMessage) => message.id));
+        case TOMATO_APP_MESSAGE_CREATE_SUCCESS:
+            return prevState.push(action.payload.message.id);
         default:
             return prevState;
     }
