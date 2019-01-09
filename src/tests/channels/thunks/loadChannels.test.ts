9788efb9ef22b00f0a1a294d1fb55bd2521883c1
Sevitas <sevcik.martin4@gmail.com>
2019-01-09 17:44:35 +0100

    loadChannels thunk test introduced

---
+++ src/tests/channels/thunks/loadChannels.test.ts
@@ -0,0 +1,39 @@
+import {
+    TOMATO_APP_LOADING_CHANNELS_STARTED,
+    TOMATO_APP_LOADING_CHANNELS_SUCCESS
+} from '../../../constants/actionTypes';
+import {authTokenHelper, channelHelper, channelServerModelResponse, dispatch} from '../helpers/helpers';
+import {
+    createLoadAllChannelFactory,
+    loadingFailed,
+    loadingStarted,
+    loadingSuccess
+} from "../../../actions/channel/loadChannels";
+
+describe('Load channels thunk tests.', () => {
+    const expectedLoadChannelsStarted = {type: TOMATO_APP_LOADING_CHANNELS_STARTED};
+
+    const expectedLoadChannelsSuccess = {type: TOMATO_APP_LOADING_CHANNELS_SUCCESS,
+        payload: {
+            channels: [channelHelper],
+        }};
+
+    const loadAllChannels = (authToken: AuthToken) => {
+        console.log(authToken);
+        return Promise.resolve({data: [channelServerModelResponse]});
+    };
+
+    const createTestLoadChannelsDependencies = {
+        loadingStarted,
+        loadingSuccess,
+        loadingFailed,
+        loadAllChannels
+    };
+
+    test('Dispatch thunks in correct order: loadChannels.', async done => {
+        await createLoadAllChannelFactory(createTestLoadChannelsDependencies)(authTokenHelper)(dispatch);
+        expect(dispatch.mock.calls[0][0]).toEqual(expectedLoadChannelsStarted);
+        expect(dispatch.mock.calls[1][0]).toEqual(expectedLoadChannelsSuccess);
+        done();
+    });
+});
