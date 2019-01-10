5be7f3b3fe4219ae5a6e2ccc991d3dd70759700d
Sevitas <sevcik.martin4@gmail.com>
2019-01-10 20:24:07 +0100

    user reducer test modifications

--- src/reducers/users/userId.ts
+++ src/reducers/users/userId.ts
@@ -1,7 +1,7 @@
 import {
     TOMATO_APP_USER_LOGIN_FAILED,
     TOMATO_APP_USER_LOGIN_SUCCESS,
-    TOMATO_APP_USER_LOGIN_STARTED, TOMATO_APP_USER_LOGOUT_SUCCESS,
+    TOMATO_APP_USER_LOGOUT_SUCCESS,
 } from '../../constants/actionTypes';
 import {IUser} from '../../models/IUser';
 
@@ -10,7 +10,6 @@ export const userId = (prevState: Uuid | null = null, action: Action): Uuid | nu
         case TOMATO_APP_USER_LOGIN_SUCCESS:
             return action.payload.user.id;
         case TOMATO_APP_USER_LOGIN_FAILED:
-        case TOMATO_APP_USER_LOGIN_STARTED:
         case TOMATO_APP_USER_LOGOUT_SUCCESS:
             return null;
         default:
