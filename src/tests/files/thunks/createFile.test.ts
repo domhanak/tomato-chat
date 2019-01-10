e7497e2e0e4dafd57a3106fc39200d7b02bf205d
Sevitas <sevcik.martin4@gmail.com>
2019-01-10 23:25:12 +0100

    added test cases for channel

--- src/tests/files/thunks/createFile.test.ts
+++ src/tests/files/thunks/createFile.test.ts
@@ -1,9 +1,10 @@
-import {authTokenHelper, dispatch} from '../../baseHelpers';
+import {authTokenHelper, rejectedPromise, resolvedPromise} from '../../baseHelpers';
 import {
     expectedGetFileStarted, fileHelper,
     fileIdHelper
 } from '../helpers/helpers';
 import {
+    TOMATO_APP_FILE_CREATE_FAILED,
     TOMATO_APP_FILE_CREATE_STARTED,
     TOMATO_APP_FILE_CREATE_SUCCESS,
 } from '../../../constants/actionTypes';
@@ -13,6 +14,7 @@ import {
     fileCreateSuccess, createAvatarCreateFactory
 } from '../../../actions/files/createFile';
 import {expectedUserChannelStarted, userServerModelHelper} from '../../users/helpers/helpers';
+import {errorMessageCreateFile} from '../../../constants/errorMessages';
 
 describe('File create thunk action tests.', () => {
     const expectedCreateFileStarted = {type: TOMATO_APP_FILE_CREATE_STARTED};
@@ -22,25 +24,45 @@ describe('File create thunk action tests.', () => {
             fileId: fileIdHelper,
         }};
 
+    const expectedCreateFileFailed = {type: TOMATO_APP_FILE_CREATE_FAILED,
+        payload: errorMessageCreateFile};
+
     const fileCreate = (file: File, authToken: AuthToken) => {
         return Promise.resolve({data: [{id: fileIdHelper}]});
     };
 
-    const createTestCreateFileDependencies = {
-        fileCreateStarted,
-        fileCreateFailed,
-        fileCreateSuccess,
-        fileCreate
+    const fileCreateReject = (file: File, authToken: AuthToken) => {
+        return Promise.reject({error: {}});
     };
 
-    test('Dispatch thunks in correct order: createFile.', async done => {
-        await createAvatarCreateFactory(createTestCreateFileDependencies)
+    const createTestCreateFileDependencies = (promise: boolean) => {
+        return {
+            fileCreateStarted,
+            fileCreateFailed,
+            fileCreateSuccess,
+            fileCreate: promise ? fileCreate : fileCreateReject,
+        };
+    };
+
+    test('Dispatch thunks in correct order, resolved: createFile.', async done => {
+        const dispatch = jest.fn((action) => action);
+        await createAvatarCreateFactory(createTestCreateFileDependencies(resolvedPromise))
             (authTokenHelper, fileHelper, userServerModelHelper)(dispatch);
 
         expect(dispatch.mock.calls[0][0]).toEqual(expectedCreateFileStarted);
         expect(dispatch.mock.calls[1][0]).toEqual(expectedUserChannelStarted);
         expect(dispatch.mock.calls[2][0]).toEqual(expectedGetFileStarted);
         expect(dispatch.mock.calls[3][0]).toEqual(expectedCreateFileSuccess);
+        done();
+    });
+
+    test('Dispatch thunks in correct order, rejected: createFile.', async done => {
+        const dispatch = jest.fn((action) => action);
+        await createAvatarCreateFactory(createTestCreateFileDependencies(rejectedPromise))
+        (authTokenHelper, fileHelper, userServerModelHelper)(dispatch);
+
+        expect(dispatch.mock.calls[0][0]).toEqual(expectedCreateFileStarted);
+        expect(dispatch.mock.calls[1][0]).toEqual(expectedCreateFileFailed);
         done();
     });
 });
--- src/tests/files/thunks/getFile.test.ts
+++ src/tests/files/thunks/getFile.test.ts
@@ -1,12 +1,14 @@
-import {authTokenHelper, dispatch} from '../../baseHelpers';
+import {authTokenHelper, rejectedPromise, resolvedPromise} from '../../baseHelpers';
 import {
     IfileHelper,
     fileIdHelper
 } from '../helpers/helpers';
 import {
+    TOMATO_APP_GET_FILE_FAILED,
     TOMATO_APP_GET_FILE_STARTED, TOMATO_APP_GET_FILE_SUCCESS,
 } from '../../../constants/actionTypes';
 import {createGetFileFactory, getFileFailed, getFileStarted, getFileSuccess} from '../../../actions/files/getFile';
+import {errorMessageGetFile} from '../../../constants/errorMessages';
 
 describe('File get thunk action tests.', () => {
     const expectedGetFileStarted = {type: TOMATO_APP_GET_FILE_STARTED};
@@ -16,23 +18,43 @@ describe('File get thunk action tests.', () => {
             file: IfileHelper,
         }};
 
+    const expectedGetFileFailed = {type: TOMATO_APP_GET_FILE_FAILED,
+        payload: errorMessageGetFile};
+
     const getFileApiCall = (fileId: Uuid, authToken: AuthToken) => {
         return Promise.resolve({data: IfileHelper});
     };
 
-    const createTestGetFileDependencies = {
-        getFileStarted,
-        getFileFailed,
-        getFileSuccess,
-        getFileApiCall
+    const getFileApiCallReject = (fileId: Uuid, authToken: AuthToken) => {
+        return Promise.reject({error: {}});
     };
 
-    test('Dispatch thunks in correct order: getFile.', async done => {
-        await createGetFileFactory(createTestGetFileDependencies)
+    const createTestGetFileDependencies = (promise: boolean) => {
+        return {
+            getFileStarted,
+            getFileFailed,
+            getFileSuccess,
+            getFileApiCall: promise ? getFileApiCall : getFileApiCallReject,
+        };
+    };
+
+    test('Dispatch thunks in correct order, resolved: getFile.', async done => {
+        const dispatch = jest.fn((action) => action);
+        await createGetFileFactory(createTestGetFileDependencies(resolvedPromise))
             (fileIdHelper, authTokenHelper)(dispatch);
 
         expect(dispatch.mock.calls[0][0]).toEqual(expectedGetFileStarted);
         expect(dispatch.mock.calls[1][0]).toEqual(expectedGetFileSuccess);
+        done();
+    });
+
+    test('Dispatch thunks in correct order, rejected: getFile.', async done => {
+        const dispatch = jest.fn((action) => action);
+        await createGetFileFactory(createTestGetFileDependencies(rejectedPromise))
+        (fileIdHelper, authTokenHelper)(dispatch);
+
+        expect(dispatch.mock.calls[0][0]).toEqual(expectedGetFileStarted);
+        expect(dispatch.mock.calls[1][0]).toEqual(expectedGetFileFailed);
         done();
     });
 });
